69ae43f75e1eb6c5e16cc62e6de9774f68b198e6
ViliusVed <vilius.vedegys@gmail.com>
2022-11-29 00:32:09 +0200

    login form update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,13 +13,20 @@ export class LoginComponent {
   
   hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  getpasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.password.hasError('password') ? 'Not a valid password' : '';
   }
 
 
